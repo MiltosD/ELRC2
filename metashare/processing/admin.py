2b09d5da9fb95518f4b99505200d11759613b7c2
MiltosD <mdeligiannis@outlook.com>
2018-11-09 12:49:02 +0200

    define action for processing object deactivation

--- metashare/processing/admin.py
+++ metashare/processing/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.auth.admin import csrf_protect_m
 from metashare.processing.models import Processing
 
 
@@ -11,3 +12,25 @@ class ProcessingObjectAdmin(admin.ModelAdmin):
                        'status', 'date_created', 'user')
 
     list_filter = ('active', 'status',)
+
+    actions = ('deactivate_processing_object',)
+
+    @csrf_protect_m
+    def deactivate_processing_object(self, request, queryset):
+        if request.user.is_superuser:
+            successful = 0
+            for obj in queryset:
+                if obj.active:
+                    obj.active = False
+                    obj.save()
+                    successful += 1
+            if successful > 0:
+                messages.info(request,
+                              'Successfully deactivated {} processing objects.'.format(successful))
+            else:
+                messages.warning(request, 'No processing objects have been marked for deactivation')
+        else:
+            messages.error(request, 'You do not have the permission to '
+                                    'perform this action for all selected resources.')
+
+    deactivate_processing_object.short_description = "Deactivate Selected Processing Objects"

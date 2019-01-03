a3fb5c61998de063e462a8693cd77ed07fb7f126
MiltosD <mdeligiannis@outlook.com>
2019-01-03 09:44:52 +0200

    add urls for listing resources

--- metashare/repository/api/urls.py
+++ metashare/repository/api/urls.py
@@ -4,5 +4,7 @@ urlpatterns = patterns(
     'metashare.repository.api.views',
     url(r'get_data/(?P<object_id>\w+)/$', 'get_data', name='get_data'),
     url(r'upload_data/(?P<object_id>\w+)/$', 'upload_data', name='upload_data'),
-    url(r'get_xml/(?P<object_id>\w+)/$', 'get_xml', name='upload_data')
+    url(r'list/$', 'list', name='list'),
+    url(r'list/my/$', 'list_my', name='list_my'),
+    url(r'get_xml/(?P<object_id>\w+)/$', 'get_xml', name='get_xml')
 )
